ef6f2c7e57c06fd272eaed38bcbf6d0f3d52d69a
Yevgen Polukov <yevgen.polukov@paycash.onmicrosoft.com>
2017-06-05 23:19:09 +0300

    clean code.

--- src/main/java/com/proxy/ProxyApplication.java
+++ src/main/java/com/proxy/ProxyApplication.java
@@ -1,12 +1,8 @@
 package com.proxy;
 
 import org.littleshoot.proxy.impl.DefaultHttpProxyServer;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class ProxyApplication {
-
-    private static final Logger LOG = LoggerFactory.getLogger(ProxyApplication.class);
 
 	public static void main(String[] args) {
         final String portValue = getProperties("server.port");
